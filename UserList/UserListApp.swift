ce904997d7220e51565b3b8d94908751ee46f3de
Jatin Mishra <jatinmishra27@gmail.com>
2022-06-11 17:27:08 +0530

    Initial Commit

---
+++ UserList/UserListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserListApp.swift
+//  UserList
+//
+//  Created by Jatin Mishra on 11/06/22.
+//
+
+import SwiftUI
+
+@main
+struct UserListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
